4fee7258d6ebb7ae7d563216d46f6f738258b4d0
caio.silva <cst_sbb_caio@minervafoods.com>
2022-01-11 20:18:36 -0300

    Added base of views, forms and url to run the application

--- dev_challenge/cnab/admin.py
+++ dev_challenge/cnab/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cnab, Store
 
-# Register your models here.
+admin.site.register(Cnab)
+admin.site.register(Store)
--- dev_challenge/cnab/views.py
+++ dev_challenge/cnab/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import FileForm
 
-# Create your views here.
+def cnab(request):
+    if request.method == "POST":
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('cnab')
+
+    return render(request=request, 
+					template_name="upload_file.html", 
+					context={'form':FileForm()})
